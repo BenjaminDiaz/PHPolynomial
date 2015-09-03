fba22227de27ce2ef2ed468053b8d0d0c1125522
BenjaminDiaz <benjamindiazf@openmailbox.org>
2015-09-03 00:52:47 -0300

    Modified regex to allow fraction coeffs

--- classes/PolynomialObject.php
+++ classes/PolynomialObject.php
@@ -1,6 +1,9 @@
 <?php
-
 /**
+ * 
+ * PolynomialObject containing terms as array of PolynomialTerms and operators as array of strings.
+ * has methods for differentiating, serializing to JSON and toString.
+ * It receives an array of terms and an array of operators as constructor params.
  *
  */
 class PolynomialObject implements JsonSerializable {
@@ -15,41 +18,28 @@ class PolynomialObject implements JsonSerializable {
 	}
 	public function solve($x) {
 	}
+	/**
+	 * Calculates derivative. It does it applying the rule: dx/dy ax^n => nax^n-1
+	 *
+	 * Returns $this, allowing chaining
+	 */
 	public function differentiate() {
-		$terms = array();
-		$operators = array();
-		$j=0;
-		for($i=0; $i < count ( $this->terms ); $i ++) {
-			$coefficient = $this->terms[$i]->coefficient * $this->terms[$i]->exponent;
-			$exponent = $this->terms[$i]->exponent - 1;
-			if($coefficient == 0 || $exponent < 0) {
+		$terms = array ();
+		$operators = array ();
+		$j = 0;
+		for($i = 0; $i < count ( $this->terms ); $i ++) {
+			$coefficient = $this->terms [$i]->coefficient * $this->terms [$i]->exponent;
+			$exponent = $this->terms [$i]->exponent - 1;
+			if ($coefficient == 0 || $exponent < 0) {
 				continue;
 			}
-			$terms[] = new PolynomialTerm($coefficient, $exponent);		
-			$j++;
+			$terms [] = new PolynomialTerm ( $coefficient, $exponent );
+			$j ++;
 		}
-		for (; $j>1; $j--) {
-			$operators[]='+';
+		for(; $j > 1; $j --) {
+			$operators [] = '+';
 		}
-		$this->derivative = new PolynomialObject($terms, $operators);
-		
-		// $derivative = '';
-		// $i = 0;
-		// for (; $i < count($this->terms) - 1 ; $i++) {
-		// $coefficient = $this->terms[$i]->coefficient * $this->terms[$i]->exponent;
-		// $exponent = $this->terms[$i]->exponent - 1;
-		// $derivative .= $coefficient.'x^'.$exponent.$this->operators[$i];
-		
-		// }
-		// $coefficient = $this->terms[$i]->coefficient * $this->terms[$i]->exponent;
-		// $exponent = $this->terms[$i]->exponent - 1;
-		// if ($exponent == 0) {
-		// $derivative .= $coefficient;
-		// }
-		// else {
-		// $derivative .= $coefficient.'x^'.$exponent;
-		// }
-		// $this->derivative = new PolynomialObject($derivative);
+		$this->derivative = new PolynomialObject ( $terms, $operators );
 	}
 	public function __toString() {
 		$str = '';
--- classes/PolynomialParser.php
+++ classes/PolynomialParser.php
@@ -1,28 +1,38 @@
 <?php
+/**
+ * Parses polynomial expression of type 'ax^z+bx^y+cx^w'
+ *
+ * Returns PolynomialObject
+ */
 class PolynomialParser {
 	public static function parse($str) {
 		$terms = array ();
-		$pattern = '/(\-*\d*)[a-z]*\^*(\d*)/';
+		$pattern = '/(\-*\d*\/*\d*)[a-z]\^*(\d*)/';
 		if (preg_match_all ( $pattern, $str, $matches, PREG_SET_ORDER )) {
 			foreach ( $matches as $match ) {
+				
 				if (! empty ( $match [0] )) {
 					// If lonely x:
 					if (empty ( $match [1] ) && empty ( $match [2] )) {
 						$coefficient = 1;
 						$exponent = 1;
-					}					// If ax:
+					} // If ax:
 					elseif ($match [0] == $match [1]) {
 						$coefficient = $match [1];
 						$exponent = 0;
-					} 					// If ax^n or x^n
+					}  // If ax^n or x^n
 					else {
 						$coefficient = (empty ( $match [1] )) ? 1 : $match [1];
 						$exponent = (empty ( $match [2] )) ? 1 : $match [2];
 					}
+					
 					$terms [] = new PolynomialTerm ( $coefficient, $exponent );
 				}
 			}
 		}
+		// For each operator in the expression it adds a + sign. Negative operators
+		// will be added directly to the coefficients
+		// Example: '5x^4-3x^5-65' => '5x^4+-3x^5+-65'
 		$operators = array ();
 		$pattern = '/\w([+-])/';
 		if (preg_match_all ( $pattern, $str, $matches, PREG_SET_ORDER )) {
@@ -31,6 +41,7 @@ class PolynomialParser {
 			}
 		}
 		$polynomialObject = new PolynomialObject ( $terms, $operators );
+		
 		return $polynomialObject;
 	}
 }
